8338083b0760704e0ae78aab9d35e68dd2eba8c7
Trent Beitz <trentb713@gmail.com>
2023-11-24 16:05:19 -0500

    have the struct working and all the variables in it initialised, the array constant size to remove any magic numbers, and a error checker to see how many commmand-line arguments are put in

--- f8.cpp
+++ f8.cpp
@@ -12,8 +12,36 @@
 #include <stdlib.h>
 #pragma warning(disable:4996)
 
+//Constants
+const int kArrayLength = 21;
 
 int main(int argc, char *argv[]) {
 
+	//the struct, it containds all the data from the comandline
+	struct MyData
+	{
+		int howMany;
+		char theText[kArrayLength];
+		char directoryPath[kArrayLength];
+		char filename[kArrayLength];
+	};
+
+	//checks to see if the user put the corect amount of 
+	if (argc != 5) {
+		if (argc > 5) {
+			printf("ERROR MORE THEN THE ACCSEPTIBEL AMOUNT OF ARGUMENTS INPUTED\nPLEASE INPUT LESS COMMAND-LINE ARGUMENTS");
+			return -1;
+		}
+		else {
+			printf("ERROR LESS THEN THE ACCSEPTIBEL AMOUNT OF ARGUMENTS INPUTED\nPLEASE INPUT MORE COMMAND-LINE ARGUMENTS");
+			return -1;
+		}
+	}
+
+	struct MyData myArgs = { 0, "", "", "" };
+	strcpy(myArgs.theText, argv[2]);
+	strcpy(myArgs.directoryPath, argv[3]);
+	strcpy(myArgs.filename, argv[4]);
+	myArgs.howMany = atoi(argv[1]);
 	
 }
