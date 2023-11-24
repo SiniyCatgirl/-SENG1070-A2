300ba8480e22a5db24d96af63e09a91cd80bcc9a
Trent Beitz <trentb713@gmail.com>
2023-11-24 15:23:55 -0500

    first commit, has the header, libraries and main

---
+++ f8.cpp
@@ -0,0 +1,19 @@
+/*
+* FILE : f4.cpp
+* PROJECT : PROG1345 - Focoused 4
+* PROGRAMMER : Trent Beitz
+* FIRST VERSION : 21-11-23
+* DESCRIPTION :
+* gives practice with C-style strings, files, command line arguments, and structs
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+
+int main(int argc, char *argv[]) {
+
+	
+}
